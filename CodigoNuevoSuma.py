11b1ef6efe383f0635e91d741848318de7bdebe5
Kendallgg <79158143+Kendallgg@users.noreply.github.com>
2025-08-16 15:27:14 -0600

    Update CodigoNuevoSuma.py
    Corrección

--- CodigoNuevoSuma.py
+++ CodigoNuevoSuma.py
@@ -1,5 +1,6 @@
 # Solicita dos números y los suma
 num1 = int(input("Ingrese el primer número: "))
-num2 =int(input("Ingrese el segundo número: "))
-resultado = num1 +num2  # realiza la suma
-print("La suma es:", resultado)  #Imprime el resultado
+num2 = int(input("Ingrese el segundo número: "))
+resultado = num1 + num2  # realiza la suma
+print("La suma es:", resultado)  # Imprime el resultado
+
